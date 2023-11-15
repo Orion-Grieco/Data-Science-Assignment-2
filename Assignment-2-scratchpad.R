7f04b2f6d2afbb354776f76c7b0bb435f9cb9d6b
Orion Grieco <Orion.grieco@Gmail.com>
2023-11-15 23:47:58 +0100

    making progress? I guess I'm getting something done

--- Assignment-2-scratchpad.R
+++ Assignment-2-scratchpad.R
@@ -3,9 +3,14 @@ load("titanic_train.RDATA")
 head(titanic.train)
 # - View data
 View(titanic.train)
-
+example <- data.frame(titanic.train$Survived, titanic.train$Fare)
+example$Survived <- titanic.train$Survived
+example$Fare <- titanic.train$Fare
+example$titanic.train.Survived <- NULL
+example$titanic.train.Fare <- NULL
 # "Data" is referenced by titanic.train
-
+View(example)
+print(example$Survived)
 nrep <- 50
 d_minsplit <- seq(from = 2, to = 40, by = 2)
 d_maxdepth <- seq(from = 1, to = 5, by = 1)
@@ -13,11 +18,26 @@ d_cp <- 10^(-seq(from = 2, to = 4, by = 1))
 parametros <- expand.grid(minsplit = d_minsplit, maxdepth = d_maxdepth, cp = d_cp) # nolint: line_length_linter.
 modelqualityresults <- data.frame(accuracy = rep(0, nrep), sensitivity = rep(0, nrep))
 
-df = data.frame(titanic.train)
+df <- data.frame(example)
 set.seed(1)
-idx_tr = sample(nrow(df),150)
-train = df[idx_tr,]
-test = df[-idx_tr,]
+nreps <- 100
+ccr = rep(0, nreps)
+idx_tr <- sample(nrow(df), 150)
+train <- df[idx_tr, ]
+test <- df[-idx_tr, ]
 library(ggplot2)
-ggplot(train) + geom_point(aes(Survived,Pclass))+xlab("Training Set X.1 Values")+ylab("Training Set X.2 Values")
-ggplot(test) + geom_point(aes(Survived,Age))+xlab("Testing Set X.1 Values")+ylab("Testing Set X.2 Values")
+ggplot(train) +
+    geom_point(aes(Survived, Fare)) +
+    xlab("Training Set X.1 Values") +
+    ylab("Training Set X.2 Values")
+ggplot(test) +
+    geom_point(aes(Survived, Age)) +
+    xlab("Testing Set X.1 Values") +
+    ylab("Testing Set X.2 Values")
+
+matrix_1 <- as.matrix(df)
+library(rpart)
+library(rpart.plot)
+tree <- rpart(Survived ~ ., data = train)
+pred <- predict(tree, newdata = test, type = "vector")
+crr[i] <- sum(pred == test$Survived) / nrow(test)
