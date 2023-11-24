d623eff40c88cfa5ea1d46c0e330a344e19b44f2
Orion Grieco <Orion.grieco@Gmail.com>
2023-11-24 22:30:58 +0100

    Update Assignment-2.R

--- Assignment-2.R
+++ Assignment-2.R
@@ -3,22 +3,30 @@ load("titanic_train.RDATA")
 head(titanic.train)
 # - View data
 View(titanic.train)
-df <- data.frame(titanic.train)
 
-create_data_sets <- function(df, size = 0.8, train = TRUE) {
-        num_rows <- nrow(df)
+library(dplyr)
+df <- data.frame(titanic.train)
+cleaned_df <- df %>% select(-c(Cabin, Ticket))
+# Making a binary indicator of cabin status
+df$CabinStatus <- ifelse(df$Cabin == "", 0, 1)
+
+cleaned_df <- df %>% select(-c(Cabin, Ticket))
+View(cleaned_df)
+
+create_data_sets <- function(cleaned_df, size = 0.8, train = TRUE) {
+        num_rows <- nrow(cleaned_df)
         total_row <- size * num_rows
         train_sample <- 1:total_row
         if (train == TRUE) {
-                return(df[train_sample, ])
+                return(cleaned_df[train_sample, ])
         } else {
-                return(df[-train_sample, ])
+                return(cleaned_df[-train_sample, ])
         }
 }
 
 
-training_data <- create_data_sets(df, 0.8, train = TRUE)
-testing_data <- create_data_sets(df, 0.8, train = FALSE)
+training_data <- create_data_sets(cleaned_df, 0.8, train = TRUE)
+testing_data <- create_data_sets(cleaned_df, 0.8, train = FALSE)
 
 # Viewing created data sets
 View(training_data)
@@ -29,4 +37,8 @@ dim(testing_data)
 prop.table(table(training_data$Survived))
 prop.table(table(testing_data$Survived))
 
-
+install.packages("rpart.plot")
+library(rpart.plot)
+library(rpart)
+tree <- rpart(Survived ~ ., data = training_data, method = "class")
+rpart.plot(tree, extra = 106)
